0deea233a717117f3406e8d6889becbbcf855c6a
avi9360 <savinash406@gmail.com>
2022-10-18 21:58:02 +0530

    Added chars

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import LineGraph from "../components/LineGraph";
 function Home() {
   const [data, setData] = useState("");
 
@@ -8,20 +8,23 @@ function Home() {
     console.log(data);
   };
   return (
-    <div className="flex flex-col justify-center items-center m-2">
-      <div className="flex flex-row">
-        <div>
-          <datalist id="suggestions">
-            <option>First option</option>
-            <option>Second Option</option>
-          </datalist>
-          <input placeholder="Search" className="p-2 border-4 border-indigo-500/75 rounded-xl ml-2 focus:outline-none focus:ring focus:border-blue-500" autoComplete="on" list="suggestions" value={data} onChange={e => setData(e.target.value)} />
+    <>
+      <div className="flex flex-col justify-center items-center m-2">
+        <div className="flex flex-row">
+          <div>
+            <datalist id="suggestions">
+              <option>First option</option>
+              <option>Second Option</option>
+            </datalist>
+            <input placeholder="Search" className="p-2 border-4 border-indigo-500/75 rounded-xl ml-2 focus:outline-none focus:ring focus:border-blue-500" autoComplete="on" list="suggestions" value={data} onChange={e => setData(e.target.value)} />
+          </div>
+          <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleSubmit}>
+            Submit
+          </button>
         </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" onClick={handleSubmit}>
-          Submit
-        </button>
+        <LineGraph />
       </div>
-    </div>
+    </>
   );
 }
 
